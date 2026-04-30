d725c6cc079bd95bda4291959790b0d23a7c8b87
sagarthapliyal <sagarthapliyal9720@gmail.com>
2026-04-30 11:19:06 +0530

    add postgress

--- config/config/settings.py
+++ config/config/settings.py
@@ -154,3 +154,13 @@ SIMPLE_JWT = {
 
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
